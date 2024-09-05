c24835bbf1601886897c93acdf56ac418db7deb0
DeathwingIN <imeshnirmal1u@gmail.com>
2024-09-05 10:24:55 +0530

    Slider with Mob error

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import Carousel from "./reusableComponents/Carousel";
+
+const images = [
+  { src: 'https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp', alt: 'Drink 1' },
+  { src: 'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp', alt: 'Drink 2' },
+  { src: 'https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp', alt: 'Drink 1' },
+  { src: 'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp', alt: 'Drink 2' },
+  { src: 'https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp', alt: 'Drink 1' },
+  { src: 'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp', alt: 'Drink 2' },
+  { src: 'https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp', alt: 'Drink 1' },
+  { src: 'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp', alt: 'Drink 2' },
+  { src: 'https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp', alt: 'Drink 1' },
+  { src: 'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp', alt: 'Drink 2' },
+ 
+  // Add more images
+];
 
 export default function Slider() {
+
+
+
   return (
     <div>
-      <h1 className='text-white'>Slider</h1>
+      <Carousel images={images} />
     </div>
-  )
+  );
 }
