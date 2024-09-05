7e627f650f6fd8723d74a75a62487de33c7ac80f
DeathwingIN <imeshnirmal1u@gmail.com>
2024-09-05 19:33:47 +0530

    About Div

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function Why() {
   return (
-    <div>
-       <h1 className='text-white'>Why</h1>
-    </div>
-  )
+    <section className="h-screen grid md:grid-cols-3 md:grid-rows-2 grid-rows-6 grid-cols-1 gap-4 md:px-20 md:pt-20 pt-8 px-8">
+      
+      <div className="bg-red-500">1
+      <h1 className="text-white font-cormorant md:text-[60px] text-[50px]  uppercase   text-center">
+        portfolio
+      </h1>
+      </div>
+      <div className="bg-green-500">2</div>
+      <div className="bg-blue-500">3</div>
+      <div className="bg-yellow-500">4</div>
+      <div className="bg-purple-500">5</div>
+      <div className="bg-orange-500">6</div>
+      
+    </section>
+  );
 }
