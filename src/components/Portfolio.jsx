9ce9fc3f7161573fcc820221a2b2230881c54e7b
DeathwingIN <imeshnirmal1u@gmail.com>
2024-09-05 17:27:45 +0530

    Gallery !

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -27,15 +27,18 @@ export default function Gallery() {
       : galleryItems.filter((item) => item.category === activeTab);
 
   return (
-    <section className="w-full h-screen p-4">
+    <section className="w-full  p-4 mt-20">
+      <h1 className="text-white font-cormorant md:text-[80px] text-[50px] leading-tight uppercase md:text-right md:mr-20 text-center">
+        portfolio
+      </h1>
       {/* Tabs */}
-      <div className="flex space-x-4 mb-4">
+      <div className="flex space-x-0 md:space-x-4 mb-4 md:mt-8 md:mb-20 md:justify-end md:mr-40 justify-center  ">
         {tabs.map((tab) => (
           <button
             key={tab}
             onClick={() => setActiveTab(tab)}
-            className={`px-4 py-2 text-white ${
-              activeTab === tab ? "bg-blue-500" : "bg-gray-300"
+            className={`px-3 py-2 ${
+              activeTab === tab ? "text-[#bfd6df]" : "text-white"
             }`}
           >
             {tab}
@@ -53,9 +56,72 @@ export default function Gallery() {
       >
         {activeTab === "All" ? (
           <>
-            <div className="bg-gray-300 p-4 h-full md:col-span-2">Div 1</div>
-            <div className="bg-gray-300 p-4 h-full md:col-span-1">Div 2</div>
-            <div className="bg-gray-300 p-4 h-full md:col-span-2">Div 3</div>
+            <div className=" p-4  grid-cols-span-2 grid grid-cols-1  gap-4 md:col-span-2">
+              <div className=" md:row-span-3 grid grid-cols-1 md:grid-cols-3">
+                <div className="col-span-1 p-4">
+                  <img 
+                    src="https://images.pexels.com/photos/371985/pexels-photo-371985.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    alt=""
+                  />
+                </div>
+                <div className="col-span-2 p-4 md:grid md:grid-rows-2  hidden  ">
+                  <div className="row-span-1 ">
+                    <img 
+                      src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                      alt=""
+                    />
+                  </div>
+                  <div className="row-span-1 ">
+                    <img 
+                      src="https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className=" md:row-span-1 hidden md:block">
+                <img  
+                  src="https://images.pexels.com/photos/262959/pexels-photo-262959.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+              </div>
+            </div>
+
+            <div className=" p-4  hidden md:block md:col-span-1">
+              <img 
+                src="https://images.pexels.com/photos/27947208/pexels-photo-27947208/free-photo-of-vietcombank-tower.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+            </div>
+            <div className=" p-4 h-full hidden md:grid grid-cols-1 gap-4 md:col-span-1">
+              <div className="md:row-span-1">
+                <img
+                  src="https://images.pexels.com/photos/1162983/pexels-photo-1162983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+              </div>
+              <div className="md:row-span-1">
+                <img
+                  src="https://images.pexels.com/photos/432059/pexels-photo-432059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className=" p-4 h-full hidden md:grid grid-cols-1 gap-4 md:col-span-1">
+              <div className=" md:row-span-1">
+                <img
+                  src="https://images.pexels.com/photos/247287/pexels-photo-247287.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+              </div>
+              <div className=" md:row-span-3">
+                <img
+                  src="https://images.pexels.com/photos/2010877/pexels-photo-2010877.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+              </div>
+            </div>
           </>
         ) : (
           filteredItems.map((item) => (
